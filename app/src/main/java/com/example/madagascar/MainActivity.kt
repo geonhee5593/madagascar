12f9d532119568ba71beb9d0e9ac6e169300dcd8
geonhee5593 <hgh717171@gmail.com>
2024-10-20 22:33:53 +0900

    건희 커밋 완료 2

--- app/src/main/java/com/example/madagascar/MainActivity.kt
+++ app/src/main/java/com/example/madagascar/MainActivity.kt
@@ -6,41 +6,42 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
+/* 축모아 메인 화면 클래스 */
 class MainActivity : AppCompatActivity() {
-    lateinit var  temp : ArrayList<Drawable>
+    /* Drawable 객체를 저장할 ArrayList 선언 */
+    lateinit var temp: ArrayList<Drawable>
 
+     /* onCreate 메서드, 액티비티가 생성될 때 호출됨 */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val favoritesBtn = findViewById<ImageView>(R.id.star1) //즐겨찾기 이동
-
+        /* btn_star1 버튼 클릭 시 즐겨찾기 화면으로 이동 */
+        val favoritesBtn = findViewById<ImageView>(R.id.star1)
         favoritesBtn.setOnClickListener {
             val intent = Intent(this, FavoritesActivity::class.java)
             startActivity(intent)
         }
 
+        /* btn_happhuy 버튼 클릭 시 월별 축제 화면으로 이동 */
         val happguyBtn = findViewById<ImageView>(R.id.btn_happguy)
-
         happguyBtn.setOnClickListener {
             val intent = Intent(this, happguyActivity::class.java)
             startActivity(intent)
         }
 
+        /* btn_mypage1 버튼 클릭 시 마이페이지 화면으로 이동 */
         val mypageBtn = findViewById<ImageView>(R.id.btn_mypage1)
-
         mypageBtn.setOnClickListener {
             val intent = Intent(this, MypageActivity::class.java)
             startActivity(intent)
         }
-        val fieldBtn = findViewById<ImageView>(R.id.btn_field)
 
+        /* btn_field 버튼 클릭 시 관심분야별 화면으로 이동 */
+        val fieldBtn = findViewById<ImageView>(R.id.btn_field)
         fieldBtn.setOnClickListener {
             val intent = Intent(this, hobby_Activity::class.java)
             startActivity(intent)
         }
     }
 }
-
-
-
