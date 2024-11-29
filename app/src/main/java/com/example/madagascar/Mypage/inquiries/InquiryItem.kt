28a2d3332e2f4121c48a4b945159e96c1e4a982d
geonhee5593 <hgh717171@gmail.com>
2024-11-29 17:56:34 +0900

    문의 수정

--- app/src/main/java/com/example/madagascar/Mypage/inquiries/InquiryItem.kt
+++ app/src/main/java/com/example/madagascar/Mypage/inquiries/InquiryItem.kt
@@ -5,6 +5,7 @@ data class InquiryItem(
     var inquiryId: String = "",
     var question: String = "",
     var timestamp: String = "",
+    var username: String = "",
     var responseStatus: String = "답변 미확인", // 기본값 설정
     var adminResponse: String? = null // 기본값 설정
 )
